e2cd225ef0262a02abaa33910e0e08dd91da5ef4
Jonathan Pho <jonpho@gmail.com>
2019-06-13 15:41:03 -0500

    Updating navigation and other site changes

--- src/components/About.js
+++ src/components/About.js
@@ -8,19 +8,18 @@ class About extends React.Component {
         <div className="row">
           <div className="col-8">
             <h2>Welcome</h2>
-            <p className="text-left">Hello! My name is Jonathan Pho. I'm a Senior Automation Engineer with over 10 years
-              of experience in managing and planning QA teams. I have an extensive
-              background in manual testing and automated testing. Along with my
-              background in testing, I have a proven track record of building,
-              managing, mentoring, and guiding QA teams. If you would like to know
-              more about my professional experience please feel free to look at my <a href="https://www.linkedin.com/in/jonpho">LinkedIN</a> profile
-              for more information.
+            <p className="text-left">Hello! My name is Jonathan Pho. I'm a Software Engineer in Test with over 10 years
+              of experience in managing and planning QA teams. I have an extensive background in automated testing and
+              manual testing. Along with my background in testing, I have a proven track record of building, managing,
+              mentoring, and guiding QA teams. If you would like to know more about my professional experience please
+              feel free to checkout my Social Network links above and contact me.
             </p>
-            <p className="text-left">The main point of this site is to update my skills and knowledge of
-              coding while sharing my code samples, or my experiences in golfing and QA.
-              In recent years coding has been a big part of my career and my goals.
-              It's a great skill to have and I hope to branch into more software
-              engineering in the future.
+            <p className="text-left">The main point of this site is to update my skills and knowledge of coding. In
+              recent years coding has been a big part of my career and my goals. It's a great skill to have and I hope
+              to branch into more software engineering in the future.
+            </p>
+            <p className="text-left">The tech stack of this site is currently created from React with Bootstrap. My goal
+              is to add more backend work to this site but that is a work in progress.
             </p>
           </div>
           <div className="col-4">
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import menuIcon from '../images/hamburger_menu_icon.png';
 
 class Navigation extends React.Component {
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <ul className="nav">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand" href="#">Jonathan Pho</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+            </li>
             <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false" href="#"><img src={menuIcon} /></a>
+              <a className="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Social Media</a>
               <div className="dropdown-menu">
                 <a className="dropdown-item" href="https://github.com/jonpho">GitHub</a>
                 <a className="dropdown-item" href="https://www.linkedin.com/in/jonpho">LinkedIN</a>
@@ -19,7 +26,23 @@ class Navigation extends React.Component {
             </li>
           </ul>
         </div>
-      </div>
+      </nav>
+      // <div className="container">
+      //   <div className="row">
+      //     <ul className="nav">
+      //       <li className="nav-item dropdown">
+      //         <a className="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false" href="#">Social Media</a>
+      //         <div className="dropdown-menu">
+      //           <a className="dropdown-item" href="https://github.com/jonpho">GitHub</a>
+      //           <a className="dropdown-item" href="https://www.linkedin.com/in/jonpho">LinkedIN</a>
+      //           <a className="dropdown-item" href="https://www.facebook.com/jonpho">Facebook</a>
+      //           <a className="dropdown-item" href="https://twitter.com/jonathanpho">Twitter</a>
+      //           <a className="dropdown-item" href="https://www.instagram.com/leftphodead/">Instagram</a>
+      //         </div>
+      //       </li>
+      //     </ul>
+      //   </div>
+      // </div>
     );
   }
 }
