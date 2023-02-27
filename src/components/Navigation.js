e5db1cf04c2acdcef8cd6ddd78ee23c3356872ba
Jonathan Pho <jonpho@gmail.com>
2023-02-27 11:13:05 -0600

    Update links

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,7 +4,7 @@ class Navigation extends React.Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" href="#">Jonathan Pho</a>
+        <a className="navbar-brand" href="/index.html">Jonathan Pho</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon" />
@@ -12,10 +12,10 @@ class Navigation extends React.Component {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
-              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+              <a className="nav-link" href="/index.html">Home <span className="sr-only">(current)</span></a>
             </li>
             <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" data-toggle="dropdown" href="#" id="navbarDropdown" role="button" aria-haspopup="true" aria-expanded="false">Social Media</a>
+              <a className="nav-link dropdown-toggle" data-toggle="dropdown" href="/index.html" id="navbarDropdown" role="button" aria-haspopup="true" aria-expanded="false">Social Media</a>
               <div className="dropdown-menu" aria-labelledby="navbarDropdown">
                 <a className="dropdown-item" href="https://github.com/jonpho">GitHub</a>
                 <a className="dropdown-item" href="https://www.linkedin.com/in/jonpho">LinkedIN</a>
