d7b60753ee6b223750331038243a0d15d3edb480
Jonathan Pho <jonathan.pho@yardstik.com>
2024-08-20 09:31:26 -0500

    Updates to site

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import About from './components/About';
+import Elect from './components/Elect';
 import Footer from "./components/Footer";
 import Jumbotron from "./components/Jumbotron";
 import Navigation from "./components/Navigation"
@@ -15,7 +15,7 @@ function App() {
         <Jumbotron />
       </div>
       <div className="section">
-        <About />
+        <Elect />
       </div>
       <div className="section">
         <Footer />
